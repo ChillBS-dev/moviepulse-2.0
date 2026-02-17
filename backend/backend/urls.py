011b36973016ee63aa59f6d53e1fe1523e125987
Dev <gabinlessent@gmail.com>
2026-02-17 16:18:43 +0100

    urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,26 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from movieapp.views import get_trending
 
 urlpatterns = [
     path("api/", include("movieapp.urls")),
     path("admin/", admin.site.urls),
+    path("api/trending/", get_trending),
 ]
+
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
-    from movieapp.views import get_trending  # ajoutez cet import
-
-urlpatterns = [
-    ...
-    path('api/trending/', get_trending),  # ajoutez cette ligne
-]
-from movieapp.views import get_trending  # ajoutez cet import
-
-urlpatterns = [
-    ...
-    path('api/trending/', get_trending),  # ajoutez cette ligne
-]
