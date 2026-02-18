dd93cb0ec2224dff137cfe1bd0388249750f1018
Dev <gabinlessent@gmail.com>
2026-02-18 08:23:19 +0100

    urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from movieapp.views import get_trending
+from movieapp.views import (
+    get_trending, 
+    change_password, 
+    update_profile, 
+    get_profile
+)
 
 urlpatterns = [
     path("api/", include("movieapp.urls")),
     path("admin/", admin.site.urls),
     path("api/trending/", get_trending),
+    path("api/change-password/", change_password),
+    path("api/profile/update/", update_profile),
+    path("api/profile/", get_profile),
 ]
 
 if settings.DEBUG:
